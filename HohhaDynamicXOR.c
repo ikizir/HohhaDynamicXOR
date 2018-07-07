495edcac4d3d386a5fa63eae685aa073819582a4
ikizir <ikizir@gmail.com>
2018-07-08 01:52:02 +0300

    Update HohhaDynamicXOR.c
    V1 is obsoleted. We are going to upload V2 files soon.

--- HohhaDynamicXOR.c
+++ HohhaDynamicXOR.c
@@ -1,4 +1,5 @@
 /*B.
+THIS FILE IS V1 OF HOHHA ALGORITHM AND THEREFORE OBSOLETE! 
 Hohha Dynamic XOR Algorithm. Copyright (c) 2015 İsmail Kizir
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
 and associated documentation files (the "Software"), to deal in the Software without restriction, 
